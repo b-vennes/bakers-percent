9d3a632e54a4c3baa4bd839d39e71a88b4ae8671
Branden <b.n.vennes@gmail.com>
2020-03-12 16:59:50 -0700

    Updates

---
+++ precache-manifest.8166efcece171e49efb1e72b201829c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a716f511886a4b288d57abb2c8b6eaf",
+    "url": "/bakers-percent/index.html"
+  },
+  {
+    "revision": "0112fb3acaae942c5a04",
+    "url": "/bakers-percent/static/css/main.8b084fcb.chunk.css"
+  },
+  {
+    "revision": "0d799b80fa77b89110e7",
+    "url": "/bakers-percent/static/js/2.56771d9a.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/bakers-percent/static/js/2.56771d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0112fb3acaae942c5a04",
+    "url": "/bakers-percent/static/js/main.499143fc.chunk.js"
+  },
+  {
+    "revision": "258f48c80dc0c46c3526",
+    "url": "/bakers-percent/static/js/runtime-main.7cc2ee23.js"
+  }
+]);
